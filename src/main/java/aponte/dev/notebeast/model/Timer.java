72661ab0937c3692ecaad5b3d44303d50404008c
Diesel Aponte <diesel.aponte@usil.pe>
2025-07-13 22:45:14 -0500

    Se han realizado cambios principalmente en el funcionamiento del timer, donde funciona actualmente, falta realizar el formato de pausa entre cambios de estado, sin embargo, esta funcionando correctamente.
    Siguiente paso: Revisar nuevos patrones de diseño.

--- src/main/java/aponte/dev/notebeast/model/Timer.java
+++ src/main/java/aponte/dev/notebeast/model/Timer.java
@@ -2,42 +2,61 @@ package aponte.dev.notebeast.model;
 
 import aponte.dev.notebeast.util.TimerState;
 import java.time.LocalDateTime;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Timer {
     private TimerState timerState;
-    private int studyDuration;
-    private int breakDuration;
+    private long studyDurationMilliseconds;
+    private long breakDurationMilliseconds;
     private long remainingTime;
     private long startTime;
     private long endTime;
 
-    public Timer(int studyDuration, int breakDuration) {
-        this.studyDuration = studyDuration;
-        this.breakDuration = breakDuration;
-        this.timerState = TimerState.STUDYING;
-        timerStart();
-        breakStart();
+    public Timer(long studyDurationMins, long breakDurationMins) {
+        studyDurationMins = studyDurationMins * 60000;
+        breakDurationMins = breakDurationMins * 60000;
+        this.studyDurationMilliseconds = studyDurationMins;
+        this.breakDurationMilliseconds = breakDurationMins;
     }
 
-    public void timerStart(){
-        System.out.println("Timer started");
-        startTime = System.currentTimeMillis();
-        endTime = studyDuration * 60L;
-        while (endTime > startTime) {
-            remainingTime = endTime - startTime;
-            System.out.println("remainng time: " + remainingTime);
-        }
-        System.out.println("Timer Finished");
+    public void startTimer(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Starting Study Timer");
+        StartStudyTimer();
+        timerState = TimerState.PAUSED;
+        String input = scanner.nextLine();
+        StartBreakTimer();
     }
 
-    public void breakStart(){
-        System.out.println("Break started");
+    private void StartStudyTimer(){
+        timerState = TimerState.STUDYING;
+        countDownTimer(studyDurationMilliseconds);
+    }
+
+    private void StartBreakTimer(){
+        timerState = TimerState.BREAK;
+        countDownTimer(breakDurationMilliseconds);
+    }
+
+    private void countDownTimer(long timeMillis){
         startTime = System.currentTimeMillis();
-        endTime = breakDuration * 60L;
-        while (endTime > startTime) {
-            remainingTime = endTime - startTime;
-            System.out.println("remainng time: " + remainingTime);
+        endTime = startTime + timeMillis;
+        while(endTime > System.currentTimeMillis()) {
+            remainingTime = endTime - System.currentTimeMillis();
+            if (timeMillis > TimeUnit.MINUTES.toMillis(60)) {
+                long hours = TimeUnit.MILLISECONDS.toHours(remainingTime);
+                long minutes = TimeUnit.MILLISECONDS.toMinutes(remainingTime) % 60;
+                long seconds = TimeUnit.MILLISECONDS.toSeconds(remainingTime) % 60;
+                System.out.printf("Tiempo restante: %02d:%02d:%02d", hours, minutes, seconds);
+                System.out.println("");
+            } else {
+                long minutes = TimeUnit.MILLISECONDS.toMinutes(remainingTime);
+                long seconds = TimeUnit.MILLISECONDS.toSeconds(remainingTime) % 60;
+                long milliseconds = remainingTime % 1000;
+                System.out.printf("Tiempo restante: %02d:%02d:%02d", minutes, seconds, milliseconds, '\n');
+                System.out.println("");
+            }
         }
-        System.out.println();
     }
 }
--- src/main/java/aponte/dev/notebeast/testing.java
+++ src/main/java/aponte/dev/notebeast/testing.java
@@ -5,8 +5,6 @@ import aponte.dev.notebeast.model.Timer;
 public class testing {
     public static void main(String[] args) {
         Timer timer = new Timer(1,1);
-        // returns the current time in milliseconds
-        System.out.print("Current Time in milliseconds = ");
-        System.out.println(System.nanoTime());
+        timer.startTimer();
     }
 }
